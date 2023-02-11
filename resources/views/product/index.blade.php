3a134c807eace7a29f3efdbaeeb984b543ed5e21
Latif Widhi Hartoko <latifwidhihartoko@gmail.com>
2023-02-11 13:07:59 +0700

    update judul kolom tabel

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -13,8 +13,8 @@
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
-                                <th scope="col" class="px-6 py-3">
-                                    Product name
+                                <th scope="col" class="px-6 py-6">
+                                    Nama Product
                                 </th>
                                 <th scope="col" class="px-6 py-3">
                                     Qty
